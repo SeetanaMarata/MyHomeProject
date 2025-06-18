e80bd6e6615af47685d8fb1b253359adcb1cbc2f
Swetlana Mirzoeva <levka008@mail.ru>
2025-06-18 18:47:24 +0300

    Add new function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -19,3 +19,14 @@ print(filter_by_state(data, 'CANCELED'))
 # Вывод: [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 # {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
+
+def sort_by_date(operations, reverse=True):
+    return sorted(operations, key=lambda x: x['date'], reverse=reverse)
+
+# Сортировка по убыванию (новые операции в начале)
+sorted_desc = sort_by_date(data)
+print(sorted_desc)
+
+# Сортировка по возрастанию (старые операции в начале)
+sorted_asc = sort_by_date(data, reverse=False)
+print(sorted_asc)
