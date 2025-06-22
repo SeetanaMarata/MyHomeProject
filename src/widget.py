edb3b525ed598e18c3a70ca449503ebdcd692078
Swetlana Mirzoeva <levka008@mail.ru>
2025-06-22 19:27:41 +0300

    fixed style one more

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from masks import get_mask_account, get_mask_card_number
+from .masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(unique_number: Union[str]) -> Union[str]:
