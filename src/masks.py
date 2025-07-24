b18a15fcdf02e7eb538a202e9c60ff94ac3a74c2
Swetlana Mirzoeva <levka008@mail.ru>
2025-07-24 13:06:51 +0300

    data masking module

--- src/masks.py
+++ src/masks.py
@@ -1,40 +1,42 @@
-from typing import Union
+from typing import List
+
+from .logger_config import setup_logger
+
+# Настройка логгера для модуля masks
+logger = setup_logger("masks", "logs/masks.log")
 
 
-def get_mask_card_number(pin_code_numbers: list[str]) -> Union[str]:
+def get_mask_card_number(pin_code_numbers: List[str]) -> str:
     """
-    Маскировка номера банковской карты
-    :param pin_code_numbers: список цифр карты
-    :return: замаскированный номер с группировкой по 4 цифры
+    Маскирует номер карты с логированием
     """
-    n = len(pin_code_numbers)
-    if n > 6:
-        # Заменяем 6 символов начиная с 6-й позиции
-        end_replace = min(12, n)  # Ограничиваем конец замены длиной номера
-        num_stars = end_replace - 6  # Количество символов для замены
-        if num_stars > 0:
-            pin_code_numbers[6:end_replace] = ["*"] * num_stars
+    try:
+        n = len(pin_code_numbers)
+        if n > 6:
+            end_replace = min(12, n)
+            num_stars = end_replace - 6
+            if num_stars > 0:
+                pin_code_numbers[6:end_replace] = ["*"] * num_stars
 
-    # Собираем строку из списка
-    code_after_masks = "".join(pin_code_numbers)
-    # Разбиваем на группы по 4 символа
-    code_for_user = [code_after_masks[i: i + 4] for i in range(0, len(code_after_masks), 4)]
-    return " ".join(code_for_user)
+        code_after_masks = "".join(pin_code_numbers)
+        code_for_user = [code_after_masks[i: i + 4] for i in range(0, len(code_after_masks), 4)]
+        result = " ".join(code_for_user)
 
-
-# Проверка работы функции
-print(get_mask_card_number(list("7000792289606361")))  # 7000 79** **** 6361
-print(get_mask_card_number(list("1234")))  # 1234
-print(get_mask_card_number(list("")))  # (пустая строка)
+        logger.info(f"Карта успешно замаскирована: {result}")
+        return result
+    except Exception as e:
+        logger.error(f"Ошибка маскировки карты: {str(e)}", exc_info=True)
+        return ""
 
 
-def get_mask_account(account_number: Union[str]) -> Union[str]:
+def get_mask_account(account_number: str) -> str:
     """
-    маскировки номера банковского счета
-    :return: замаскированный счет
+    Маскирует номер счета с логированием
     """
-    account_can_be_show = "**" + account_number[-4:]
-    return account_can_be_show
-
-
-print(get_mask_account("73654108430135874305"))
+    try:
+        result = "**" + account_number[-4:]
+        logger.info(f"Счет успешно замаскирован: {result}")
+        return result
+    except Exception as e:
+        logger.error(f"Ошибка маскировки счета: {str(e)}", exc_info=True)
+        return ""
